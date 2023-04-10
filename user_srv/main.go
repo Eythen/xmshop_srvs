3ae353f711ced45d8314bbbb94d09a87ba9edd2c
陈亿辛 <327527333@qq.com>
2023-04-10 11:23:46 +0800

    通过flag启动grpc服务

--- user_srv/main.go
+++ user_srv/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+	"xmshop_srvs/user_srv/handler"
+	"xmshop_srvs/user_srv/proto"
+)
 
+func main() {
+	IP := flag.String("ip", "0.0.0.0", "IP address")
+	Port := flag.Int("port", 50051, "Port number")
+
+	flag.Parse()
+	fmt.Println("ip:", *IP)
+	fmt.Println("port:", *Port)
+
+	server := grpc.NewServer()
+	proto.RegisterUserServer(server, &handler.UserServer{})
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *IP, *Port))
+	if err != nil {
+		panic("failed to listen:" + err.Error())
+	}
+	err = server.Serve(lis)
+	if err != nil {
+		panic("failed to start grpc:" + err.Error())
+	}
 }
