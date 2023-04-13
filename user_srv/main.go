8991e0b0533a92c6bf1ae8987a1fa34749217a12
陈亿辛 <327527333@qq.com>
2023-04-13 15:03:44 +0800

    将grpc注册到consul中

--- user_srv/main.go
+++ user_srv/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/hashicorp/consul/api"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"net"
+	"xmshop_srvs/user_srv/global"
 	"xmshop_srvs/user_srv/handler"
 	"xmshop_srvs/user_srv/initialize"
 	"xmshop_srvs/user_srv/proto"
@@ -34,6 +36,38 @@ func main() {
 	}
 	//注册服务健康检查
 	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	//服务注册
+	cfg := api.DefaultConfig()
+	cfg.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
+
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+	//生成对应的检查对象
+	check := &api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("192.168.0.103:%d", *Port),
+		Timeout:                        "5s",
+		Interval:                       "5s",
+		DeregisterCriticalServiceAfter: "15s",
+	}
+
+	//生成注册对象
+	registration := new(api.AgentServiceRegistration)
+	registration.Name = global.ServerConfig.Name
+	registration.ID = global.ServerConfig.Name
+	registration.Port = *Port
+	registration.Tags = []string{"imooc", "bobby", "user", "srv"}
+	registration.Address = "192.168.0.103"
+	registration.Check = check
+
+	//1.如何启动两个服务
+	//2.即使通过终端启动两个服务，但是注册到consul中的时候也会被覆盖掉
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		panic(err)
+	}
 
 	err = server.Serve(lis)
 	if err != nil {
