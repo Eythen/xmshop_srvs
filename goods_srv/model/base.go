39127d35feaffde2fad86b0ea2f48760d7f08b6e
陈亿辛 <327527333@qq.com>
2023-04-14 15:23:42 +0800

    商品表结构

--- goods_srv/model/base.go
+++ goods_srv/model/base.go
@@ -1,9 +1,22 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"gorm.io/gorm"
 	"time"
 )
+
+type GormList []string
+
+func (g GormList) Value() (driver.Value, error) {
+	return json.Marshal(g)
+}
+
+// 实现sql. Scanner接口, Scan将value 扫描至Jsonb
+func (g *GormList) Scan(value interface{}) error {
+	return json.Unmarshal(value.([]byte), &g)
+}
 
 type BaseModel struct {
 	ID        int32     `gorm:"primaryKey;type:int"` //为什么使用int32
--- goods_srv/model/goods.go
+++ goods_srv/model/goods.go
@@ -38,3 +38,28 @@ type Banner struct {
 	Url   string `gorm:"type:varchar(200);not null comment '跳转链接'"`
 	Index int32  `gorm:"type:int;default:1;not null comment '排序'"`
 }
+
+type Goods struct {
+	BaseModel
+	CategoryID int32 `gorm:"type:int;not null comment '分类id'"`
+	Category   Category
+	BrandsID   int32 `gorm:"type:int;not null comment '品牌id'"`
+	Brands     Brands
+
+	OnSale   bool `gorm:"default:false;not null comment '是否上架'"`
+	ShipFree bool `gorm:"default:false;not null comment '是否免运费'"`
+	IsNew    bool `gorm:"default:false;not null comment '是否新品'"`
+	IsHot    bool `gorm:"default:false;not null comment '是否热卖商品'"`
+
+	Name            string   `gorm:"type:varchar(50);not null comment '名称'"`
+	GoodsSn         string   `gorm:"type:varchar(50);not null comment '商品编号'"`
+	ClickNum        int32    `gorm:"type:int;default:0;not null comment '点击量'"`
+	SoldNum         int32    `gorm:"type:int;default:0;not null comment '销量'"`
+	FavNum          int32    `gorm:"type:int;default:0;not null comment '收藏数'"`
+	MarketPrice     float32  `gorm:"not null comment '市场价'"`
+	ShopPrice       float32  `gorm:"not null comment '价格'"`
+	GoodsBrief      string   `gorm:"type:varchar(100);not null comment '商品简介'"`
+	Images          GormList `gorm:"type:varchar(1000);not null comment '图册'"`
+	DescImages      GormList `gorm:"type:varchar(1000);not null comment '商品详情图'"`
+	GoodsFrontImage string   `gorm:"type:varchar(200);not null comment '商品主图'"`
+}
