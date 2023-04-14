95ca01fcd266778320b89d4929e3da7e979711f5
陈亿辛 <327527333@qq.com>
2023-04-14 16:21:39 +0800

    注释问题

--- goods_srv/model/goods.go
+++ goods_srv/model/goods.go
@@ -6,17 +6,17 @@ package model
 
 type Category struct {
 	BaseModel
-	Name             string `gorm:"type:varchar(20);not null comment '名称'"`
+	Name             string `gorm:"type:varchar(20);comment:名称;not null"`
 	ParentCategoryID int32
 	ParentCategory   *Category
-	Level            int32 `gorm:"type:int;not null;default:1 comment '等级'"`
-	IsTab            bool  `gorm:"default:false;not null comment '是否展现在tab栏'"`
+	Level            int32 `gorm:"type:int;default:1;not null;comment:等级"`
+	IsTab            bool  `gorm:"default:false;not null;comment:是否展示在tab"`
 }
 
 type Brands struct {
 	BaseModel
-	Name string `gorm:"type:varchar(20);not null comment '名称'"`
-	Logo string `gorm:"type:varchar(200);default:'';not null comment '名称'"`
+	Name string `gorm:"type:varchar(20);comment:名称;not null"`
+	Logo string `gorm:"type:varchar(200);default:'';comment:名称;not null"`
 }
 
 type GoodsCategoryBrand struct {
@@ -34,32 +34,32 @@ type GoodsCategoryBrand struct {
 
 type Banner struct {
 	BaseModel
-	Image string `gorm:"type:varchar(200);not null comment '图片'"`
-	Url   string `gorm:"type:varchar(200);not null comment '跳转链接'"`
-	Index int32  `gorm:"type:int;default:1;not null comment '排序'"`
+	Image string `gorm:"type:varchar(200);comment:图片;not null"`
+	Url   string `gorm:"type:varchar(200);comment:跳转链接;not null"`
+	Index int32  `gorm:"type:int;default:1;comment:排序;not null"`
 }
 
 type Goods struct {
 	BaseModel
-	CategoryID int32 `gorm:"type:int;not null comment '分类id'"`
+	CategoryID int32 `gorm:"type:int;not null;comment:分类id;"`
 	Category   Category
-	BrandsID   int32 `gorm:"type:int;not null comment '品牌id'"`
+	BrandsID   int32 `gorm:"type:int;not null;comment:品牌id"`
 	Brands     Brands
 
-	OnSale   bool `gorm:"default:false;not null comment '是否上架'"`
-	ShipFree bool `gorm:"default:false;not null comment '是否免运费'"`
-	IsNew    bool `gorm:"default:false;not null comment '是否新品'"`
-	IsHot    bool `gorm:"default:false;not null comment '是否热卖商品'"`
+	OnSale   bool `gorm:"default:false;not null;comment:是否上架;"`
+	ShipFree bool `gorm:"default:false;not null;comment:是否免运费"`
+	IsNew    bool `gorm:"default:false;not null;comment:是否新品"`
+	IsHot    bool `gorm:"default:false;not null;comment:是否热卖商品"`
 
-	Name            string   `gorm:"type:varchar(50);not null comment '名称'"`
-	GoodsSn         string   `gorm:"type:varchar(50);not null comment '商品编号'"`
-	ClickNum        int32    `gorm:"type:int;default:0;not null comment '点击量'"`
-	SoldNum         int32    `gorm:"type:int;default:0;not null comment '销量'"`
-	FavNum          int32    `gorm:"type:int;default:0;not null comment '收藏数'"`
-	MarketPrice     float32  `gorm:"not null comment '市场价'"`
-	ShopPrice       float32  `gorm:"not null comment '价格'"`
-	GoodsBrief      string   `gorm:"type:varchar(100);not null comment '商品简介'"`
-	Images          GormList `gorm:"type:varchar(1000);not null comment '图册'"`
-	DescImages      GormList `gorm:"type:varchar(1000);not null comment '商品详情图'"`
-	GoodsFrontImage string   `gorm:"type:varchar(200);not null comment '商品主图'"`
+	Name            string   `gorm:"type:varchar(50);not null;comment:名称"`
+	GoodsSn         string   `gorm:"type:varchar(50);not null;comment:商品编号"`
+	ClickNum        int32    `gorm:"type:int;default:0;not null;comment:点击量"`
+	SoldNum         int32    `gorm:"type:int;default:0;not null;comment:销量"`
+	FavNum          int32    `gorm:"type:int;default:0;not null;comment:收藏数"`
+	MarketPrice     float32  `gorm:"not null;comment:市场价"`
+	ShopPrice       float32  `gorm:"not null;comment:价格"`
+	GoodsBrief      string   `gorm:"type:varchar(100);not null;comment:商品简介"`
+	Images          GormList `gorm:"type:varchar(1000);not null;comment:图册"`
+	DescImages      GormList `gorm:"type:varchar(1000);not null;comment:商品详情图"`
+	GoodsFrontImage string   `gorm:"type:varchar(200);not null;comment:商品主图"`
 }
