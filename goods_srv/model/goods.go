fdc922b41f20f4ce907a68e8325d7854a345fe22
陈亿辛 <327527333@qq.com>
2023-04-14 14:59:32 +0800

    品牌轮播图表结构

--- goods_srv/model/goods.go
+++ goods_srv/model/goods.go
@@ -12,3 +12,29 @@ type Category struct {
 	Level            int32 `gorm:"type:int;not null;default:1 comment '等级'"`
 	IsTab            bool  `gorm:"default:false;not null comment '是否展现在tab栏'"`
 }
+
+type Brands struct {
+	BaseModel
+	Name string `gorm:"type:varchar(20);not null comment '名称'"`
+	Logo string `gorm:"type:varchar(200);default:'';not null comment '名称'"`
+}
+
+type GoodsCategoryBrand struct {
+	BaseModel
+	CategoryID int32 `gorm:"type:int;index:idx_category_brand,unique'"`
+	Category   Category
+	BrandsID   int32 `gorm:"type:int;index:idx_category_brand,unique'"`
+	Brands     Brands
+}
+
+//// 重置表名
+//func (GoodsCategoryBrand) TableName() string {
+//	return "goodscategorybrand"
+//}
+
+type Banner struct {
+	BaseModel
+	Image string `gorm:"type:varchar(200);not null comment '图片'"`
+	Url   string `gorm:"type:varchar(200);not null comment '跳转链接'"`
+	Index int32  `gorm:"type:int;default:1;not null comment '排序'"`
+}
